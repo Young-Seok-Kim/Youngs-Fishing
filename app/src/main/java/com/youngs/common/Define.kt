f6e3d828ca000e62c81722c9091f1f2388d76da8
Young-Seok-Kim <x1682@naver.com>
2022-09-18 02:04:20 +0900

    특정위치에 Pin을 꽂는 기능 추가, 일단은 현재위치에 꽂게 해놓음

---
+++ app/src/main/java/com/youngs/common/Define.kt
@@ -0,0 +1,10 @@
+package com.youngs.common
+
+import android.Manifest
+import android.text.InputFilter
+import java.util.regex.Pattern
+
+object Define {
+    val PERMISSIONS_REQUEST_CODE = 100
+    var REQUIRED_PERMISSIONS = arrayOf<String>( Manifest.permission.ACCESS_FINE_LOCATION)
+}
--- app/src/main/java/com/youngs/youngsfishing/MainActivity.kt
+++ app/src/main/java/com/youngs/youngsfishing/MainActivity.kt
@@ -13,7 +13,10 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.youngs.common.Define.PERMISSIONS_REQUEST_CODE
+import com.youngs.common.Define.REQUIRED_PERMISSIONS
 import com.youngs.youngsfishing.databinding.ActivityMainBinding
+import net.daum.mf.map.api.MapPOIItem
 import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
 
@@ -21,8 +24,7 @@ import net.daum.mf.map.api.MapView
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
 
-    val PERMISSIONS_REQUEST_CODE = 100
-    var REQUIRED_PERMISSIONS = arrayOf<String>( Manifest.permission.ACCESS_FINE_LOCATION)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,38 +32,67 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val mapView = MapView(this)
-
         val mapViewContainer = binding.frameLayout
-
         mapViewContainer.addView(mapView)
+        goToNowLocation(mapView)
+
+        setPin(mapView)
 
         binding.nowLocation.setOnClickListener(){
-            val permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-            if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
-                val lm: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-                try {
-                    val userNowLocation: Location =
-                        lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)!!
-                    val uLatitude = userNowLocation.latitude
-                    val uLongitude = userNowLocation.longitude
-                    val uNowPosition = MapPoint.mapPointWithGeoCoord(uLatitude, uLongitude)
-                    mapView.setMapCenterPoint(uNowPosition, true)
-                }catch(e: NullPointerException){
-                    Log.e("LOCATION_ERROR", e.toString())
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                        ActivityCompat.finishAffinity(this)
-                    }else{
-                        ActivityCompat.finishAffinity(this)
-                    }
+            goToNowLocation(mapView)
+        }
+    }
 
-                    val intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                    System.exit(0)
+    private fun setPin(mapView: MapView){
+        val permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            val lm: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            val userNowLocation: Location =
+                lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)!!
+            val uLatitude = userNowLocation.latitude
+            val uLongitude = userNowLocation.longitude
+
+            val myLocation: MapPoint = MapPoint.mapPointWithGeoCoord(uLatitude, uLongitude)
+
+            val marker: MapPOIItem = MapPOIItem()
+
+            marker.setItemName("Default Marker")
+            marker.setTag(0)
+            marker.setMapPoint(myLocation)
+            marker.setMarkerType(MapPOIItem.MarkerType.BluePin) // 기본으로 제공하는 BluePin 마커 모양.
+            marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin) // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+
+            mapView.addPOIItem(marker)
+        }
+    }
+
+    private fun goToNowLocation(mapView : MapView)
+    {
+        val permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            val lm: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            try {
+                val userNowLocation: Location =
+                    lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)!!
+                val uLatitude = userNowLocation.latitude
+                val uLongitude = userNowLocation.longitude
+                val uNowPosition = MapPoint.mapPointWithGeoCoord(uLatitude, uLongitude)
+                mapView.setMapCenterPoint(uNowPosition, true)
+            }catch(e: NullPointerException){
+                Log.e("LOCATION_ERROR", e.toString())
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    ActivityCompat.finishAffinity(this)
+                }else{
+                    ActivityCompat.finishAffinity(this)
                 }
-            }else{
-                Toast.makeText(this, "위치 권한이 없습니다.", Toast.LENGTH_SHORT).show()
-                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE )
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                System.exit(0)
             }
+        }else{
+            Toast.makeText(this, "위치 권한이 없습니다.", Toast.LENGTH_SHORT).show()
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE )
         }
     }
 }
