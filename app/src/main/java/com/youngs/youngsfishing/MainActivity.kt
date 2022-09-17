dba6300941b79c1c1b622e3cb73f247e343f6227
Young-Seok-Kim <x1682@naver.com>
2022-09-18 01:45:37 +0900

    지도 현재 위치로 이동하는 FAB 버튼 추가

--- app/src/main/java/com/youngs/youngsfishing/MainActivity.kt
+++ app/src/main/java/com/youngs/youngsfishing/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.youngs.youngsfishing
 
-import android.content.pm.PackageInfo
+import android.Manifest
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.os.Build
 import android.os.Bundle
-import android.util.Base64
 import android.util.Log
-import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.youngs.youngsfishing.databinding.ActivityMainBinding
+import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
-import java.security.MessageDigest
-import java.security.NoSuchAlgorithmException
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
+
+    val PERMISSIONS_REQUEST_CODE = 100
+    var REQUIRED_PERMISSIONS = arrayOf<String>( Manifest.permission.ACCESS_FINE_LOCATION)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,28 +31,36 @@ class MainActivity : AppCompatActivity() {
 
         val mapView = MapView(this)
 
-        val mapViewContainer = findViewById<ViewGroup>(R.id.map_view)
+        val mapViewContainer = binding.frameLayout
 
         mapViewContainer.addView(mapView)
 
-//        getHashKey()
-    }
+        binding.nowLocation.setOnClickListener(){
+            val permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
+                val lm: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                try {
+                    val userNowLocation: Location =
+                        lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)!!
+                    val uLatitude = userNowLocation.latitude
+                    val uLongitude = userNowLocation.longitude
+                    val uNowPosition = MapPoint.mapPointWithGeoCoord(uLatitude, uLongitude)
+                    mapView.setMapCenterPoint(uNowPosition, true)
+                }catch(e: NullPointerException){
+                    Log.e("LOCATION_ERROR", e.toString())
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                        ActivityCompat.finishAffinity(this)
+                    }else{
+                        ActivityCompat.finishAffinity(this)
+                    }
 
-    private fun getHashKey() {
-        var packageInfo: PackageInfo? = null
-        try {
-            packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
-        } catch (e: PackageManager.NameNotFoundException) {
-            e.printStackTrace()
-        }
-        if (packageInfo == null) Log.e("KeyHash", "KeyHash:null")
-        for (signature in packageInfo!!.signatures) {
-            try {
-                val md = MessageDigest.getInstance("SHA")
-                md.update(signature.toByteArray())
-                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT))
-            } catch (e: NoSuchAlgorithmException) {
-                Log.e("KeyHash", "Unable to get MessageDigest. signature=$signature", e)
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    System.exit(0)
+                }
+            }else{
+                Toast.makeText(this, "위치 권한이 없습니다.", Toast.LENGTH_SHORT).show()
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE )
             }
         }
     }
