f54252966d4da4542e347d5ebd146f49a6389bfa
Young-Seok-Kim <x1682@naver.com>
2022-09-18 15:12:21 +0900

    마커 생성 및 마커 클릭시 이벤트 기능구현 완료

--- app/src/main/java/com/youngs/youngsfishing/MainActivity.kt
+++ app/src/main/java/com/youngs/youngsfishing/MainActivity.kt
@@ -9,6 +9,7 @@ import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -16,6 +17,7 @@ import androidx.core.content.ContextCompat
 import com.youngs.common.Define.PERMISSIONS_REQUEST_CODE
 import com.youngs.common.Define.REQUIRED_PERMISSIONS
 import com.youngs.youngsfishing.databinding.ActivityMainBinding
+import net.daum.mf.map.api.CalloutBalloonAdapter
 import net.daum.mf.map.api.MapPOIItem
 import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
@@ -23,27 +25,38 @@ import net.daum.mf.map.api.MapView
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
-
-
+    lateinit var mapView : MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater,null,false)
         setContentView(binding.root)
+        mapView = MapView(this)
 
-        val mapView = MapView(this)
+        mapView.setCalloutBalloonAdapter(CustomBalloonAdapter(layoutInflater))
+
         val mapViewContainer = binding.frameLayout
+
         mapViewContainer.addView(mapView)
-        goToNowLocation(mapView)
+        goToNowLocation()
 
-        setPin(mapView)
+        setPin()
 
+        initButton()
+
+    }
+
+    private fun initButton() {
         binding.nowLocation.setOnClickListener(){
-            goToNowLocation(mapView)
+            goToNowLocation()
+        }
+
+        binding.addPin.setOnClickListener(){
+            setPin()
         }
     }
 
-    private fun setPin(mapView: MapView){
+    private fun setPin(){
         val permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
         if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
             val lm: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
@@ -51,22 +64,22 @@ class MainActivity : AppCompatActivity() {
                 lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)!!
             val uLatitude = userNowLocation.latitude
             val uLongitude = userNowLocation.longitude
-
             val myLocation: MapPoint = MapPoint.mapPointWithGeoCoord(uLatitude, uLongitude)
 
             val marker: MapPOIItem = MapPOIItem()
-
-            marker.setItemName("Default Marker")
-            marker.setTag(0)
-            marker.setMapPoint(myLocation)
-            marker.setMarkerType(MapPOIItem.MarkerType.BluePin) // 기본으로 제공하는 BluePin 마커 모양.
-            marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin) // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
-
+            marker.apply {
+                setItemName("Default Marker")
+                setTag(0)
+                setMapPoint(myLocation)
+                setMarkerType(MapPOIItem.MarkerType.BluePin) // 기본으로 제공하는 BluePin 마커 모양.
+                setSelectedMarkerType(MapPOIItem.MarkerType.RedPin) // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+                isDraggable = true
+            }
             mapView.addPOIItem(marker)
         }
     }
 
-    private fun goToNowLocation(mapView : MapView)
+    private fun goToNowLocation()
     {
         val permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
         if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
